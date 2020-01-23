afd742f6705098519b92f4794bcd25582cc297bd
Jacob Tate <jacob87608@gmail.com>
2020-01-23 11:12:36 -0600

    added files and an array to the js

---
+++ assets/javascript/app.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+
+var topics = ["dodge challenger", "ford mustang", "dodge viper", "chevrolet camaro", "ford gt", "chevrolet corvette", "dodge charger", "chevrolet chevelle"]
+
+
+
+});
