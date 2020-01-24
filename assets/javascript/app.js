246119c5c460e84b40d4aa9b8cb54e2e87262b03
Jacob Tate <jacob87608@gmail.com>
2020-01-24 11:09:49 -0600

    got the paused gifs playing onclick but not in the correct location

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-
+    
+   
     //the array of cars
 var topics = ["dodge challenger", "ford mustang", "dodge viper", "chevrolet camaro", "ford gt", "chevrolet corvette", "dodge charger", "chevrolet chevelle"];
 for (var i = 0; i < topics.length; i++) {
@@ -9,10 +10,10 @@ for (var i = 0; i < topics.length; i++) {
   //puts the buttons on the screen
       $(".buttonDiv").append(btn)
       btn.attr("data-car", topics[i])
-      btn.attr("class", "btn")
+      btn.attr("class", "btn btn-secondary")
       
   }
-$(".btn").on("click", function(){
+  $(".btn").on("click", function(){
 var name = $(this).attr("data-car");
 var queryURL =  "https://api.giphy.com/v1/gifs/search?q=" + name + "&api_key=KCgM79ZpxcllM9MB9ntE6v2cO7qMkabl&limit=10"
 
@@ -21,22 +22,41 @@ url: queryURL,
 method: "GET"
 })
 .then(function(response){
-    
+   
+    //results is the path to the array for the for loop
     var results = response.data;
     console.log(results)
+    
     for (var i = 0; i < results.length; i++) {
+        //creates an image tag and stores it
         var carImg = $("<img>")
+        //
         var stillGifUrl = results[i].images.fixed_height_still.url;
         var moveGifUrl = results[i].images.fixed_height.url;
+          
         var stillGif = carImg.attr("src", stillGifUrl);
-        
-      
-
+    
+        //adds data-state to all of the images
+        stillGif.attr("data-state", "still")
+        //adds a class to all of the images
+        stillGif.attr("class", "gif")
+        stillGif.attr("data-still", stillGifUrl)
+        stillGif.attr("data-move", moveGifUrl)
         $(".gifsHere").append(stillGif);
     }
+    
+$(".gif").on("click", function(){
+    var state = $(this).attr("data-state");
+    var moveGif = $(this).attr("data-move");
+    console.log(this)
+    if (state === "still") {
+         carImg.attr("src", moveGif)
+    }
+});
     console.log(name)
-    $(".gifsHere").append(moveGifUrl);
 });
 
 });
+
+
 });
