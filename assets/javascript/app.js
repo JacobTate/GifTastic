2229d664229b194cb866565804cd930bfa896f34
Jacob Tate <jacob87608@gmail.com>
2020-01-27 09:17:09 -0600

    finished the cards and styled them added more cars to the array

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
     //the array of cars
-    topics = ["dodge challenger", "ford mustang", "dodge viper", "chevrolet camaro", "ford gt", "chevrolet corvette", "dodge charger", "chevrolet chevelle", "Porsche Cayman GTS", "BMW i8 Coupe", "Audi R8", "mclaren 720s", "lamborghini aventador", "ferrari 458", "porsche 918 spyder"];
+    topics = ["dodge challenger", "ford mustang", "dodge viper", "chevrolet camaro", "ford gt", "chevrolet corvette", "dodge charger", "chevrolet chevelle",
+     "Porsche Cayman GTS", "BMW i8 Coupe", "Audi R8", "mclaren 720s", "lamborghini aventador", "ferrari 458", "porsche 918 spyder", "hellcat", "mclaren f1",
+    "ferrari f40", "Bugatti Chiron"];
     //the function to display the buttons
     function cars() {
         //clears all of the buttons for the new ones
@@ -22,7 +24,7 @@ $(document).ready(function () {
     cars();
     //the function to show the gifs
     function gifRender() {
-        var limit = 10; //$("#limit").val();
+        var limit = 10;
         //when a button is clicked store the data-car in a variable
         var name = $(this).attr("data-car");
         //use the name variable in the url
@@ -35,46 +37,48 @@ $(document).ready(function () {
             })
             //the code inside the then function runs the ajax response comes back
             .then(function (response) {
-                console.log(response);
                 //results is the path to the array for the for loop
                 var results = response.data;
                 //the loop for the array     
                 for (var i = 0; i < results.length; i++) {
                     //the rating
                     var rating = $("<p>")
+                    //gives the p tag a class for the bootstrap card
                     rating.attr("class", "card-text")
                     rating.text("rating: " + results[i].rating);
-                    //creates a bootstrap card for the gifs
+
+                    //creates a bootstrap card for the gif
                     var card = $("<div>").attr("class", "card")
                     //creates an image tag and stores it
                     var carImg = $("<img>")
+                    //adds the image tag to the card
                     card.prepend(carImg)
+                    //adds a card body for the rating
                     var cardbody = $("<div>").attr("class", "card-body")
+                    //adds the rating to the card
                     cardbody.prepend(rating)
-                    card.append(cardbody)
-
-
-                    
+                    //puts it all togther for the final card
+                    card.prepend(cardbody)
 
                     //stores the path to each still image in a variable
                     var stillGifUrl = results[i].images.fixed_height_still.url;
                     //stores the path to each moving image in a variable
                     var moveGifUrl = results[i].images.fixed_height.url;
                     //givers the img tag a src of the still image 
-                    var stillGif = carImg.attr("src", stillGifUrl);
+                    var Gif = carImg.attr("src", stillGifUrl);
                     //adds data-state to all of the images
-                    stillGif.attr("data-state", "still")
+                    Gif.attr("data-state", "still")
                     //adds a class to all of the images
-                    stillGif.attr("class", "gif")
+                    Gif.attr("class", "gif")
                     //adds data-still of the still url to all of the images
-                    stillGif.attr("data-still", stillGifUrl)
+                    Gif.attr("data-still", stillGifUrl)
                     //adds data-move of the moving url to all of the images
-                    stillGif.attr("data-move", moveGifUrl);
+                    Gif.attr("data-move", moveGifUrl);
+
                     //displays the images onto the screen
-
                     $(".gifsHere").prepend(card);
-
                 }
+
                 //onclick of one of the gifs
                 $(".gif").on("click", function (event) {
                     //prevents default
